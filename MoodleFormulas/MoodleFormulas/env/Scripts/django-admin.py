01b0b1849a22b20b0092264fe09604928d60d8da
Григорий <52451620+GrigoriyVS@users.noreply.github.com>
2021-03-16 18:22:35 +0300

    site and lisener clic

---
+++ MoodleFormulas/MoodleFormulas/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Гриша\FormulasMoodle\MoodleFormulas\MoodleFormulas\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
